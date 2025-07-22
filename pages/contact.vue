14f38bf535c83b388d0864f5b3b87c5150f4b2f3
Marc Ducobu <marc.ducobu@nccn.fgov.be>
2025-07-22 23:42:02 +0200

    Adding translation for contact & volunteer page

--- pages/contact.vue
+++ pages/contact.vue
@@ -4,9 +4,9 @@
         <section class="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-3xl mx-auto">
             <!-- Main Contact Card spanning two cols -->
             <div class="bg-off-white px-6 py-6 rounded-xl shadow overflow-hidden sm:col-span-2 text-center">
-                <h2 class="text-lg font-bold mb-4">Contactez-nous</h2>
+                <h2 class="text-lg font-bold mb-4">{{ $t('contact.contactUs') }}</h2>
                 <p class="text-sm text-neutral-dark mb-4">
-                    Pour toute demande, vous pouvez nous envoyer un courriel à
+                    {{ $t('contact.askByEmailTo') }}
                     <a href="mailto:info@foss4g.be" class="text-primary-dark underline">info@foss4g.be</a>.
                 </p>
                 <div class="text-center">
@@ -14,14 +14,14 @@
                         href="mailto:info@foss4g.be"
                         class="inline-block border-2 border-primary text-primary font-semibold px-4 py-2 rounded-lg hover:bg-primary hover:text-white transition text-sm"
                     >
-                        Envoyer un email
+                        {{ $t('contact.sendAnEmail') }}
                     </a>
                 </div>
             </div>
 
             <!-- Social Links Card spanning two cols -->
             <div class="bg-off-white px-6 py-6 rounded-xl shadow overflow-hidden sm:col-span-2">
-                <h3 class="text-lg font-bold mb-2">Suivez-nous</h3>
+                <h3 class="text-lg font-bold mb-2">{{ $t('contact.followUs') }}</h3>
                 <ul class="list-inside list-disc space-y-2 ml-4 text-sm text-neutral-dark">
                     <li>
                         LinkedIn:
--- pages/volunteer.vue
+++ pages/volunteer.vue
@@ -4,17 +4,16 @@
         <section class="grid grid-cols-1 sm:grid-cols-2 gap-6 max-w-3xl mx-auto">
             <!-- Volunteer Card spanning two cols -->
             <div class="bg-off-white px-6 py-6 rounded-xl shadow overflow-hidden sm:col-span-2">
-                <h2 class="text-lg font-bold mb-4">À la recherche de bénévoles</h2>
+                <h2 class="text-lg font-bold mb-4">{{ $t('volunteer.title') }}</h2>
                 <p class="text-sm text-neutral-dark mb-4">
-                    Nous recherchons des bénévoles pour nous aider à organiser le FOSS4G Belgique. Si vous êtes intéressé,
-                    merci de nous contacter pour rejoindre l'équipe et contribuer au succès de l'événement.
+                    {{ $t('volunteer.content') }}
                 </p>
                 <div class="text-center">
                     <NuxtLinkLocale
                         to="/contact"
                         class="inline-block border-2 border-primary text-primary font-semibold px-4 py-2 rounded-lg hover:bg-primary hover:text-white transition text-sm"
                     >
-                        Nous contacter
+                        {{ $t('volunteer.contactUs') }}
                     </NuxtLinkLocale>
                 </div>
             </div>
