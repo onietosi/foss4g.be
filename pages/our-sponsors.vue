946606ee751959e0a64c12c2f2ee65dd4f42b8fa
Olmo Nieto <7080811+onietosi@users.noreply.github.com>
2025-08-07 14:05:31 +0200

    Add TomTom to our-sponsors.vue

--- pages/our-sponsors.vue
+++ pages/our-sponsors.vue
@@ -57,6 +57,13 @@ const sponsors_silver = [
         logo: '/images/sponsors/geosolutions-logo.jpg',
         bgClass: 'bg-white',
     },
+    {
+        id: 5,
+        name: 'TomTom',
+        link: 'https://www.tomtom.com/',
+        logo: '/images/sponsors/tomtom_logo.svg',
+        bgClass: 'bg-white',
+    },
 ]
 
 const sponsors_bronze = [
